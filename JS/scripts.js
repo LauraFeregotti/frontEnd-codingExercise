ac7853f7b9d6a3d0c4aa5667ca4a37b26902c502
unknown <laura.feregotti@gmail.com>
2019-06-09 20:03:09 +0200

    Creating a POST request

---
+++ JS/scripts.js
@@ -0,0 +1,23 @@
+let messages = document.getElementById("messages");
+let textbox = document.getElementById("textbox");
+let button = document.getElementById("button");
+
+let apiUrlForGet = 'http://localhost:5000/api/v1.0?token=';
+let url = 'http://localhost:5000/api/v1.0';
+let author = 'Tom';
+let token = 'laura-test';
+
+function sendData() {
+
+    response = fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ author: author, message: textbox.value }),
+      headers: {
+        'Content-Type': 'application/json',
+        'token': token
+      }
+    }).then(response => { if (response.ok) getMessages() });
+  }
+  
+  button.addEventListener('click', sendData);
+  
