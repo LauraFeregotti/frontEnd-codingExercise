6b6cdf3299d67e1d0a4f10e9239b966a06997807
unknown <laura.feregotti@gmail.com>
2019-06-09 20:04:41 +0200

    Creating a GET request

--- JS/scripts.js
+++ JS/scripts.js
@@ -20,4 +20,27 @@ function sendData() {
   }
   
   button.addEventListener('click', sendData);
+
+  
+  function getMessages() {
+    endpoint = `${apiUrlForGet}${token}`;
+    fetch(endpoint)
+      .then(function (response) {
+        return response.json();
+  
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        for (let index = 0; index < myJson.length; index++) {
+          let newMessage = document.createElement("li", { is : 'expanding-list' });
+          newMessage.innerHTML = myJson[index].message + "    " + myJson[index].author + "   " + myJson[index].time;
+          messages.appendChild(newMessage);
+          textbox.value = "";
+          document.replaceChild("li", { is : 'expanding-list' }, " ");
+        }
+        
+  
+      });
+  
+  };
   
